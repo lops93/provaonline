3ba8f5e4400489d576bde49f389c8bd86bcf70f3
Viviam <vivian.lopesg@gmail.com>
2019-07-05 16:56:39 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include ('page/header.html') ?>
+<body>
+<?php include('page/menu.php'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+           <a href="questao.php"> <h1>Gerar Questão</h1></a>
+            <i class="fa fa-file-text" aria-hidden="true"></i>
+
+        </div>
+        <div class="col-md-6">
+<h1>Gerar Prova</h1>
+            <i class="fa fa-file-text" aria-hidden="true"></i>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
